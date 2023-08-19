141b7cea0d1d603474bc51e50eb662433047db79
Dan Anstis <dan@bsod.co.nz>
2023-08-19 13:23:03 +1000

    feat: update tests

--- tests/sync/test_sync.py
+++ tests/sync/test_sync.py
@@ -3,24 +3,31 @@ from ado_asana_sync.sync.sync import work_item
 
 
 class TestWorkItem(unittest.TestCase):
-    def test_asana_notes_link(self):
-        # Create a work_item instance with dummy data
-        item = work_item(
+    def setUp(self) -> None:
+        self.test_item = work_item(
             ado_id=123,
             title="Test Title",
-            type="Test Type",
+            type="User Story",
             status="Test Status",
             description="Test Description",
-            url="https://testurl.com",
+            url="https://testurl.example",
         )
 
+    def test_asana_notes_link(self):
         # Call the asana_notes_link method
-        result = item.asana_notes_link()
+        result = self.test_item.asana_notes_link()
 
         # Assert that the output is as expected
         self.assertEqual(
-            result, '<a href="http://testurl.com">Test Type 123</a>: Test Title'
+            result, '<a href="https://testurl.example">User Story 123</a>: Test Title'
         )
+
+    def test_asana_title(self):
+        # Call the asana_notes_link method
+        result = self.test_item.asana_title()
+
+        # Assert that the output is as expected
+        self.assertEqual(result, "User Story 123: Test Title")
 
 
 if __name__ == "__main__":
