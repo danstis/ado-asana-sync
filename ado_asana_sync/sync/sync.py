e950e17b9b4949b74ead9f32f66af3f2c4aae058
Dan Anstis <dan@bsod.co.nz>
2023-08-20 16:09:10 +1000

    feat: assign asana tasks (#32)
    # Checklist
    
    - [x] Assign labels to PR
    - [x] Request PR update using `@CodiumAI-Agent /describe`
    - [x] Request PR review using `@CodiumAI-Agent /review` (and if required
    update with `@CodiumAI-Agent /review -i`)

--- ado_asana_sync/sync/sync.py
+++ ado_asana_sync/sync/sync.py
@@ -1,73 +1,70 @@
 import os
 import json
+import logging
 import asana
+from asana import UserResponse
 from asana.rest import ApiException
 from azure.devops.v7_0.work.models import TeamContext
-from pprint import pprint
+from azure.devops.v7_0.work_item_tracking.models import WorkItem
 from ado_asana_sync.sync.app import app
 
 
-def sync_project(a: app, project):
-    # Log the item being synced
-    print(
-        f'syncing from {project["adoProjectName"]}/{project["adoTeamName"]} -> {project["asanaWorkspaceName"]}/{project["asanaProjectName"]}'
-    )
+class work_item:
+    # https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/work-items/get-work-item?view=azure-devops-rest-7.1&tabs=HTTP#examples
+    def __init__(
+        self,
+        ado_id,
+        title,
+        item_type,
+        status,
+        description,
+        url=None,
+        assigned_to=None,
+        priority=None,
+        due_date=None,
+        created_date=None,
+        updated_date=None,
+    ) -> None:
+        self.ado_id = ado_id
+        self.title = title
+        self.item_type = item_type
+        self.status = status
+        self.description = description
+        self.url = url
+        self.assigned_to = assigned_to
+        self.priority = priority
+        self.due_date = due_date
+        self.created_date = created_date
+        self.updated_date = updated_date
 
-    # Get the ADO project by name
-    ado_project = a.ado_core_client.get_project(project["adoProjectName"])
-    # pprint(ado_project)
+    def __str__(self) -> str:
+        """
+        Return a string representation of the object.
 
-    # Get the ADO team by name within the ADO project
-    ado_team = a.ado_core_client.get_team(
-        project["adoProjectName"], project["adoTeamName"]
-    )
-    # pprint(ado_team)
+        :return: The string representation of the object.
+        :rtype: str
+        """
+        return self.asana_title
 
-    # Get the Asana workspace ID by name
-    asana_workspace_id = get_asana_workspace(a, project["asanaWorkspaceName"])
-    # pprint(asana_workspace_id)
+    @property
+    def asana_title(self) -> str:
+        """
+        Generate the title of an Asana object.
 
-    # Get the Asana project by name within the Asana workspace
-    asana_project = get_asana_project(
-        a, asana_workspace_id, project["asanaProjectName"]
-    )
-    # pprint(asana_project)
+        Returns:
+            str: The formatted title of the Asana object.
+        """
+        return f"{self.item_type} {self.ado_id}: {self.title}"
 
-    # Get the backlog items for the ADO project and team
-    ado_items = a.ado_work_client.get_backlog_level_work_items(
-        TeamContext(team_id=ado_team.id, project_id=ado_project.id),
-        "Microsoft.RequirementCategory",
-    )
-    # pprint(ado_items)
+    @property
+    def asana_notes_link(self) -> str:
+        """
+        Generate the notes of an Asana object.
 
-    # Loop through each backlog item
-    for wi in ado_items.work_items:
-        # Get the work item from the ID
-        ado_task = a.ado_wit_client.get_work_item(wi.target.id)
-        current_work_item = work_item(
-            ado_id=ado_task.id,
-            title=ado_task.fields["System.Title"],
-            description=ado_task.fields.get("System.Description"),
-            status=ado_task.fields["System.State"],
-            type=ado_task.fields["System.WorkItemType"],
-            created_date=ado_task.fields["System.CreatedDate"],
-            priority=ado_task.fields["Microsoft.VSTS.Common.Priority"],
-            url=ado_task.url,
-        )
-        # Get the corresponding Asana task by name
-        asana_task = get_asana_task(a, asana_project, current_work_item.asana_title())
-        if asana_task == None:
-            # The Asana task does not exist, create it
-            print(f"creating task {current_work_item.asana_title()}")
-            create_asana_task(
-                a,
-                asana_project,
-                current_work_item,
-            )
-        else:
-            # The Asana task exists, update it
-            print(f"updating task {current_work_item.asana_title()}")
-            update_asana_task(a, asana_task.gid, current_work_item)
+        Returns:
+            str: The formatted notes of the Asana object.
+        """
+        return f'<a href="{self.url}">{self.item_type} {self.ado_id}</a>: {self.title}'
 
 
 def read_projects() -> list:
@@ -86,7 +83,119 @@ def read_projects() -> list:
                 "asanaProjectName": project["asanaProjectName"],
             }
         )
-        return projects
+    return projects
+
+
+def sync_project(a: app, project):
+    # Log the item being synced
+    logging.info(
+        f'syncing from {project["adoProjectName"]}/{project["adoTeamName"]} -> {project["asanaWorkspaceName"]}/{project["asanaProjectName"]}'
+    )
+
+    # Get the ADO project by name
+    ado_project = a.ado_core_client.get_project(project["adoProjectName"])
+
+    # Get the ADO team by name within the ADO project
+    ado_team = a.ado_core_client.get_team(
+        project["adoProjectName"], project["adoTeamName"]
+    )
+
+    # Get the Asana workspace ID by name
+    asana_workspace_id = get_asana_workspace(a, project["asanaWorkspaceName"])
+
+    # Get all Asana users in the workspace, this will enable user matching.
+    asana_users = get_asana_users(a, asana_workspace_id)
+
+    # Get the Asana project by name within the Asana workspace
+    asana_project = get_asana_project(
+        a, asana_workspace_id, project["asanaProjectName"]
+    )
+
+    # Get the backlog items for the ADO project and team
+    ado_items = a.ado_work_client.get_backlog_level_work_items(
+        TeamContext(team_id=ado_team.id, project_id=ado_project.id),
+        "Microsoft.RequirementCategory",
+    )
+
+    # Loop through each backlog item
+    for wi in ado_items.work_items:
+        # Get the work item from the ID
+        ado_task = a.ado_wit_client.get_work_item(wi.target.id)
+
+        # Skip this item if is not assigned, or the assignee does not match an Asana user.
+        ado_assigned_email = get_task_user_email(ado_task)
+        if ado_assigned_email == None:
+            logging.info(
+                f"skipping item as it is not assigned {ado_task.fields['System.Title']}"
+            )
+            continue
+        asana_matched_user = matching_user(asana_users, ado_assigned_email)
+        if asana_matched_user == None:
+            continue
+
+        current_work_item = work_item(
+            ado_id=ado_task.id,
+            title=ado_task.fields["System.Title"],
+            description=ado_task.fields.get("System.Description"),
+            status=ado_task.fields["System.State"],
+            item_type=ado_task.fields["System.WorkItemType"],
+            created_date=ado_task.fields["System.CreatedDate"],
+            priority=ado_task.fields["Microsoft.VSTS.Common.Priority"],
+            url=ado_task.url,
+            assigned_to=asana_matched_user.gid,
+        )
+
+        # Get the corresponding Asana task by name
+        asana_task = get_asana_task(a, asana_project, current_work_item.asana_title)
+        if asana_task == None:
+            # The Asana task does not exist, create it
+            logging.info(f"creating task {current_work_item.asana_title}")
+            create_asana_task(
+                a,
+                asana_project,
+                current_work_item,
+            )
+        else:
+            # The Asana task exists, update it
+            logging.info(f"updating task {current_work_item.asana_title}")
+            update_asana_task(
+                a,
+                asana_task.gid,
+                current_work_item,
+            )
+
+
+def get_task_user_email(task: WorkItem) -> str:
+    """
+    Return the email address of the user assigned to the Azure DevOps work item.
+    If no user is assigned, then return None.
+
+    Args:
+        task (WorkItem): The Azure DevOps work item object.
+
+    Returns:
+        str: The email address of the user assigned to the work item. If no user is assigned, it returns None.
+    """
+    assigned_user = task.fields.get("System.AssignedTo", None)
+    return assigned_user.get("uniqueName", None) if assigned_user else None
+
+
+def matching_user(user_list: list[UserResponse], email: str) -> UserResponse | None:
+    """
+    Check if a given email exists in a list of user objects.
+
+    Args:
+        user_list (list[UserResponse]): A list of UserResponse objects representing users.
+        email (str): A string representing the email to search for.
+
+    Returns:
+        UserResponse: The matching asana user.
+        None: If no matching user is found.
+    """
+    for user in user_list:
+        if user.email == email:
+            return user
+    return None
 
 
 def get_asana_workspace(a: app, name) -> str:
@@ -104,7 +213,7 @@ def get_asana_workspace(a: app, name) -> str:
             if w.name == name:
                 return w.gid
     except ApiException as e:
-        print("Exception when calling WorkspacesApi->get_workspaces: %s\n" % e)
+        logging.error("Exception when calling WorkspacesApi->get_workspaces: %s\n" % e)
 
 
 def get_asana_project(a: app, workspace_gid, name) -> str:
@@ -124,7 +233,7 @@ def get_asana_project(a: app, workspace_gid, name) -> str:
             if p.name == name:
                 return p.gid
     except ApiException as e:
-        print("Exception when calling ProjectsApi->get_projects: %s\n" % e)
+        logging.error("Exception when calling ProjectsApi->get_projects: %s\n" % e)
 
 
 def get_asana_task(a: app, asana_project, task_name) -> object:
@@ -141,12 +250,33 @@ def get_asana_task(a: app, asana_project, task_name) -> object:
     api_instance = asana.TasksApi(a.asana_client)
     try:
         # Get all tasks in the project
-        api_response = api_instance.get_tasks(project=asana_project)
+        opt_fields = [
+            "assignee_section",
+            "due_at",
+            "name",
+            "completed_at",
+            "tags",
+            "dependents",
+            "projects",
+            "completed",
+            "permalink_url",
+            "parent",
+            "assignee",
+            "assignee_status",
+            "num_subtasks",
+            "modified_at",
+            "workspace",
+            "due_on",
+        ]
+        api_response = api_instance.get_tasks(
+            project=asana_project,
+            opt_fields=opt_fields,
+        )
         for t in api_response.data:
             if t.name == task_name:
                 return t
     except ApiException as e:
-        print("Exception when calling TasksApi->get_tasks_in_project: %s\n" % e)
+        logging.error("Exception when calling TasksApi->get_tasks_in_project: %s\n" % e)
 
 
 def create_asana_task(a: app, asana_project: "str", task: "work_item"):
@@ -154,101 +284,79 @@ def create_asana_task(a: app, asana_project: "str", task: "work_item"):
     Create an Asana task in the specified project.
 
     Args:
+        a (app): An instance of the 'app' class that provides the connection to ADO and Asana.
         asana_project (str): The name of the Asana project to create the task in.
-        task (work_item): The work item object containing the details of the task.
+        task (work_item): An instance of the 'work_item' class that contains the details of the task to be created.
 
     Returns:
         None
-
-    Raises:
-        ApiException: If an error occurs while creating the task.
-
     """
     tasks_api_instance = asana.TasksApi(a.asana_client)
     body = asana.TasksBody(
         {
-            "name": task.asana_title(),
+            "name": task.asana_title,
             "due_on": task.due_date,
-            "html_notes": f"<body>{task.asana_notes_link()}</body>",
+            "html_notes": f"<body>{task.asana_notes_link}</body>",
             "projects": [asana_project],
+            "assignee": task.assigned_to,
         }
     )
     try:
-        api_response = tasks_api_instance.create_task(body)
-        # pprint(api_response)
+        tasks_api_instance.create_task(body)
     except ApiException as e:
-        print("Exception when calling TasksApi->create_task: %s\n" % e)
+        logging.error("Exception when calling TasksApi->create_task: %s\n" % e)
 
 
-def update_asana_task(a: app, asana_task_id: "str", task: "work_item"):
+def update_asana_task(a: app, asana_task_id: str, task: work_item):
+    """
+    Update an Asana task with the provided task details.
+
+    Args:
+        a (app): An instance of the app class that provides the connection to ADO and Asana.
+        asana_task_id (str): The ID of the Asana task to be updated.
+        task (work_item): An instance of the work_item class that contains the details of the task to be updated.
+
+    Returns:
+        None: The function does not return any value. The Asana task is updated with the provided details.
+    """
     tasks_api_instance = asana.TasksApi(a.asana_client)
     body = asana.TasksBody(
         {
-            "name": task.asana_title(),
+            "name": task.asana_title,
             "due_on": task.due_date,
-            "html_notes": f"<body>{task.asana_notes_link()}</body>",
+            "html_notes": f"<body>{task.asana_notes_link}</body>",
+            "assignee": task.assigned_to,
         }
     )
 
     try:
-        api_response = tasks_api_instance.update_task(body, asana_task_id)
-        # pprint(api_response)
+        tasks_api_instance.update_task(body, asana_task_id)
     except ApiException as e:
-        print("Exception when calling TasksApi->update_task: %s\n" % e)
+        logging.error("Exception when calling TasksApi->update_task: %s\n" % e)
 
 
-# Models
-class work_item:
-    # https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/work-items/get-work-item?view=azure-devops-rest-7.1&tabs=HTTP#examples
-    def __init__(
-        self,
-        ado_id,
-        title,
-        type,
-        status,
-        description,
-        url=None,
-        assigned_to=None,
-        priority=None,
-        due_date=None,
-        created_date=None,
-        updated_date=None,
-    ) -> None:
-        self.ado_id = ado_id
-        self.title = title
-        self.type = type
-        self.status = status
-        self.description = description
-        self.url = url
-        self.assigned_to = assigned_to
-        self.priority = priority
-        self.due_date = due_date
-        self.created_date = created_date
-        self.updated_date = updated_date
+def get_asana_users(a: app, asana_workspace_gid: str) -> list[UserResponse]:
+    """
+    Retrieves a list of Asana users in a specific workspace.
 
-    def __str__(self) -> str:
-        """
-        Return a string representation of the object.
+    Args:
+        a (app): An instance of the `app` class that provides the Asana API client.
+        asana_workspace_gid (str): The ID of the Asana workspace to retrieve users from.
 
-        :return: The string representation of the object.
-        :rtype: str
-        """
-        return self.asana_title()
+    Returns:
+        list(asana.UserResponse): A list of `asana.UserResponse` objects representing the Asana users in the specified workspace.
+    """
+    users_api_instance = asana.UsersApi(a.asana_client)
+    opt_fields = [
+        "email",
+        "name",
+    ]
 
-    def asana_title(self) -> str:
-        """
-        Generate the title of an Asana object.
-
-        Returns:
-            str: The formatted title of the Asana object.
-        """
-        return f"{self.type} {self.ado_id}: {self.title}"
-
-    def asana_notes_link(self) -> str:
-        """
-        Generate the notes of an Asana object.
-
-        Returns:
-            str: The formatted notes of the Asana object.
-        """
-        return f'<a href="{self.url}">{self.type} {self.ado_id}</a>: {self.title}'
+    try:
+        api_response = users_api_instance.get_users(
+            workspace=asana_workspace_gid,
+            opt_fields=opt_fields,
+        )
+        return api_response.data
+    except ApiException as e:
+        logging.error("Exception when calling UsersApi->get_users: %s\n" % e)
