f037d8faf0153577ec1328e4077524c53fc64ee7
Dan Anstis <dan@bsod.co.nz>
2023-08-20 10:58:41 +1000

    tests: add more tests for the work_item class

--- tests/sync/test_sync.py
+++ tests/sync/test_sync.py
@@ -22,12 +22,59 @@ class TestWorkItem(unittest.TestCase):
             result, '<a href="https://testurl.example">User Story 123</a>: Test Title'
         )
 
-    def test_asana_title(self):
-        # Call the asana_notes_link method
-        result = self.test_item.asana_title()
+    # Tests that the asana_title() method returns the correct formatted title when all parameters are provided
+    def test_asana_title_with_all_parameters(self):
+        # Create a work_item object with all parameters provided
+        work_item_obj = work_item(
+            ado_id=1,
+            title="Test Title",
+            item_type="Task",
+            status="In Progress",
+            description="Test Description",
+            url="https://example.com",
+            assigned_to="John Doe",
+            priority="High",
+            due_date="2022-01-01",
+            created_date="2021-01-01",
+            updated_date="2021-12-31",
+        )
 
-        # Assert that the output is as expected
-        self.assertEqual(result, "User Story 123: Test Title")
+        # Assert that the asana_title() method returns the correct formatted title
+        assert work_item_obj.asana_title() == "Task 1: Test Title"
+
+    # Tests that the asana_title() method returns the correct formatted title when only mandatory parameters are provided
+    def test_asana_title_with_mandatory_parameters(self):
+        # Create a work_item object with only mandatory parameters provided
+        work_item_obj = work_item(
+            ado_id=1,
+            title="Test Title",
+            item_type="Task",
+            status="In Progress",
+            description="Test Description",
+        )
+
+        # Assert that the asana_title() method returns the correct formatted title
+        assert work_item_obj.asana_title() == "Task 1: Test Title"
+
+    # Tests that the asana_title() method returns the correct formatted title when all parameters are provided with empty values
+    def test_asana_title_with_empty_values(self):
+        # Create a work_item object with all parameters provided as empty values
+        work_item_obj = work_item(
+            ado_id=None, title="", item_type="", status="", description=""
+        )
+
+        # Assert that the asana_title() method returns the correct formatted title
+        assert work_item_obj.asana_title() == " None: "
+
+    # Tests that the asana_title() method returns the correct formatted title when all parameters are provided with invalid values
+    def test_asana_title_with_invalid_values(self):
+        # Create a work_item object with all parameters provided with invalid values
+        work_item_obj = work_item(
+            ado_id="invalid", title=123, item_type=True, status=None, description=456
+        )
+
+        # Assert that the asana_title() method returns the correct formatted title
+        assert work_item_obj.asana_title() == "True invalid: 123"
 
 
 if __name__ == "__main__":
