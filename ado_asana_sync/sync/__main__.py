65970c97e99a23eb297ff4330c7194bc1bc4d11a
Dan Anstis <dan@bsod.co.nz>
2023-09-03 14:10:21 +1000

    refactor: remove connect from main

--- ado_asana_sync/sync/__main__.py
+++ ado_asana_sync/sync/__main__.py
@@ -7,10 +7,5 @@ from .sync import read_projects, sync_project
 # Create an instance of the app configuration and connect to the services.
 log_level = os.environ.get("LOG_LEVEL", "INFO")
 logging.basicConfig(level=log_level)
-app_config = App()
-try:
-    app_config.connect()
-except Exception as e:
-    logging.error("Failed to connect: %s", e)
-
-app_config.start()
+app = App()
+app.start()
