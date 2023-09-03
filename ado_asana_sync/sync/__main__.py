9c6295979a77b88dc195782eb78dea6928ca6522
Dan Anstis <dan@bsod.co.nz>
2023-09-03 14:13:10 +1000

    aider: Throw a meaningful error if the `log_level` is not valid rather than overriding to INFO.
    # Aider chat conversation:
    
    USER: update to throw a meaningfule error if the `log_level` is not valid rather than overridding to INFO
    
    ASSISTANT: To throw a meaningful error if the `log_level` is not valid, we need to modify the `ado_asana_sync\sync\__main__.py` file.
    
    Here are the steps:
    
    1. Import the `ValueError` class.
    2. If the `LOG_LEVEL` environment variable is not a valid log level, raise a `ValueError` with a meaningful message.
    
    Here are the changes:
    
    ```python
    ado_asana_sync\sync\__main__.py
    <<<<<<< HEAD
    if not isinstance(getLevelName(log_level), int):
        log_level = "INFO"
    =======
    if not isinstance(getLevelName(log_level), int):
        raise ValueError(f"Invalid log level: {log_level}")
    >>>>>>> updated
    ```
    
    This change raises a `ValueError` with a meaningful message if the `LOG_LEVEL` environment variable is not a valid log level. The `getLevelName` function returns the numeric value for a log level if it's valid, or the input string if it's not. So, if `getLevelName(log_level)` is not an integer, `log_level` is not a valid log level.

--- ado_asana_sync/sync/__main__.py
+++ ado_asana_sync/sync/__main__.py
@@ -8,7 +8,7 @@ from logging import getLevelName
 
 log_level = os.environ.get("LOG_LEVEL", "INFO")
 if not isinstance(getLevelName(log_level), int):
-    log_level = "INFO"
+    raise ValueError(f"Invalid log level: {log_level}")
 logging.basicConfig(level=log_level)
 app = App()
 app.start()
