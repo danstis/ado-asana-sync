1fa73a0e02d9a1ffc764864e570cf00e8dfb8839
Dan Anstis <dan@bsod.co.nz>
2023-09-03 14:12:05 +1000

    aider: Added a check to ensure the LOG_LEVEL environment variable contains a valid log level value.
    # Aider chat conversation:
    
    USER: add a check to ensure the LOG_LEVEL environment veriable contains a valid log level value
    
    ASSISTANT: To add a check to ensure the `LOG_LEVEL` environment variable contains a valid log level value, we need to modify the `ado_asana_sync\sync\__main__.py` file.
    
    Here are the steps:
    
    1. Import the `logging` module's `getLevelName` function.
    2. Check if the `LOG_LEVEL` environment variable is a valid log level. If it's not, set it to a default value.
    
    Here are the changes:
    
    ```python
    ado_asana_sync\sync\__main__.py
    <<<<<<< HEAD
    log_level = os.environ.get("LOG_LEVEL", "INFO")
    =======
    from logging import getLevelName
    
    log_level = os.environ.get("LOG_LEVEL", "INFO")
    if not isinstance(getLevelName(log_level), int):
        log_level = "INFO"
    >>>>>>> updated
    ```
    
    This change checks if the `LOG_LEVEL` environment variable is a valid log level. If it's not, it sets `log_level` to `"INFO"`. The `getLevelName` function returns the numeric value for a log level if it's valid, or the input string if it's not. So, if `getLevelName(log_level)` is not an integer, `log_level` is not a valid log level.

--- ado_asana_sync/sync/__main__.py
+++ ado_asana_sync/sync/__main__.py
@@ -4,7 +4,11 @@ import os
 from .app import App
 
 # Create an instance of the app configuration and connect to the services.
+from logging import getLevelName
+
 log_level = os.environ.get("LOG_LEVEL", "INFO")
+if not isinstance(getLevelName(log_level), int):
+    log_level = "INFO"
 logging.basicConfig(level=log_level)
 app = App()
 app.start()
