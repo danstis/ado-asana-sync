f02e2d935a3f2a9abd81bb4801568b65b72b8631
Dan Anstis <dan@bsod.co.nz>
2023-09-03 14:10:50 +1000

    chore: remove unused imports

--- ado_asana_sync/sync/__main__.py
+++ ado_asana_sync/sync/__main__.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 from .app import App
-from .sync import read_projects, sync_project
 
 # Create an instance of the app configuration and connect to the services.
 log_level = os.environ.get("LOG_LEVEL", "INFO")
